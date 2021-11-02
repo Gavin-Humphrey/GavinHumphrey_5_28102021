807cbab3598b087f70f1cd70b50876f4ed483c67
Gavin Humphrey <gmonie@hotmail.fr>
2021-11-02 15:00:07 -0700

    Étape 4 : Faire le lien entre un produit de la page d’accueil et la page Produit

--- front/js/index.js
+++ front/js/index.js
@@ -1,12 +1,12 @@
 const allProductsURL = 'http://localhost:3000/api/products'
 
-fetch(allProductsURL).then(res => res.json()).then(res => {  /*Utilisé le GET pour afficher les produits sur la page d'accueil*/
+fetch(allProductsURL).then(res => res.json()).then(res => {  //Utilisé le GET pour afficher les produits sur la page d'accueil
 
 const itemsSection = document.querySelector('#items');
 res.forEach((el) => {
-
+    
 const itemLink = document.createElement('a');
-itemLink.href = `./front/html/product.html?id=${el._id}`
+itemLink.href = `./product.html?id=${el._id}`
 itemLink.innerHTML = ` <article>
 <img src=${el.imageUrl} alt=${el.altTxt}>
 <h3 class="productName">${el.name}</h3>
@@ -16,3 +16,4 @@ itemsSection.appendChild(itemLink)
 return itemLink
 })
 })
+
--- front/js/product.js
+++ front/js/product.js
@@ -1,11 +1,51 @@
-fetch('http://localhost:3000/api/products')/*Envoyer une requête pour récupérer les données au format json*/
+fetch('http://localhost:3000/api/products')//Envoyer une requête pour récupérer les données au format json
 .then(response => response.json())
 .then(data => console.log(data));
 
-fetch('http://localhost:3000/api/products/107fb5b75607497b96722bda5b504926')
-.then(response => response.json())
-.then(data => console.log(data));
+// récupération de l'id
+const searchParams = new URLSearchParams(location.search);
+const id = searchParams.get("id");
 
-POST('http://localhost:3000/api/products/order')
+
+fetch('http://localhost:3000/api/products/'+id)
 .then(response => response.json())
-.then(data => console.log(data));
+.then(data => {
+    const product = data;
+     
+    printProduit(product);
+
+  // fonction pour afficher le  produit
+  function printProduit(product) {
+
+    // insertion des information de la card du produit
+    const selectionProductImage = document.getElementById("productImage");
+    selectionProductImage.innerHTML += `
+    <img src="${product.imageUrl}" alt="${product.name}">`;
+
+    const selectionProducttitle = document.getElementById("title");
+    selectionProducttitle.innerHTML += `
+      ${product.name}
+`;
+          const selectionProductPrice = document.getElementById("price");
+            selectionProductPrice.innerHTML += `
+         ${product.price}
+        `;
+            const selectionProductDescription = document.getElementById("description");
+            selectionProductDescription.innerHTML += `
+        
+        ${product.description}
+              
+        `;
+        displayColors(product);
+        }
+
+//cette fonction, parcour la liste de couleur de ce produit et les affiches dans un element HTML de type <option> 
+    function displayColors(product) {
+        const colorsChoice = document.getElementById("colors");
+        for (let colors of product.colors) {
+            colorsChoice.innerHTML += `<option value="${colors}">${colors}</option>`;
+        }
+    }
+
+});
+
